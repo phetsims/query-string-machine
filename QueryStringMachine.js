22016e2ad23de78ec28156d6ad887248278173dc
Chris Malley <cmalley@pixelzoom.com>
2016-09-09 11:02:45 -0600

    Various changes made during review. Mostly doc and validation, but some logic fixes. #2

--- QueryStringMachine.js
+++ QueryStringMachine.js
@@ -1,5 +1,8 @@
 // Copyright 2016, University of Colorado Boulder
 
+//TODO Since we don't have control over what the user enters for the parameter string, assertions herein should probably be changed to Errors
+//TODO Does this need a namespace? Does this repo have a namespace?
+//TODO Can't lint this, repo is missing standard files (Gruntfile, .gitignore, ...)
 /**
  * Query String parser that supports type coercion, defaults, error checking, etc. based on a schema.
  * See QueryStringMachine.get for the description of a schema.
@@ -9,21 +12,30 @@
 window.QueryStringMachine = (function() {
   'use strict';
 
+  // valid values for the 'type' field in the schema that describes a query parameter
+  var VALID_TYPES = [
+    'boolean', // value is either 'true' or 'false', e.g. showAnswer=true
+    'flag', // value is true if present, false if absent. If a value is supplied, it is parsed like 'boolean'.
+    'number', // value is a number, e.g. frameRate=100
+    'string', // value is a string, e.g. name=Ringo
+    'array' //TODO document
+  ];
+
   var QueryStringMachine = {
 
     /**
-     * Returns the value for a single element.
-     * @param key
-     * @param {Object} schemaElement * required: 'type' or 'parse'
-     *                                   * type: one of 'boolean'|'number'|'string'|'flag'|'array'
-     *                                   * parse: a function that takes a string and returns an object
-     *                               * optional: 'defaultValue' - The value to take if no query parameter is provided
-     *                               * optional: 'allowedValues' - Array of the allowed values for validation
-     *                               * optional: 'validate' - function that takes a parsed object (not string) and checks if it is acceptable
-     *                               * optional: 'elementType' - when type==='array' the elementType is specified here
-     *                               * optional: 'separator' - when type==='array' the elements are separated by this string
+     * Returns the value for a single query parameter.
      *
-     * @returns {number}
+     * @param {string} key - the query parameter name
+     * @param {Object} schemaElement - describes the query parameter, has these fields:
+     *   type - see VALID_TYPES
+     *   parse - a function that takes a string and returns an Object
+     *   (type and parse are mutually exclusive)
+     *   [defaultValue] - The value to take if no query parameter is provided
+     *   [allowedValues] - Array of the allowed values for validation
+     *   [validate] - function that takes a parsed Object (not string) and checks if it is acceptable
+     *   (allowedValues and validate are mutually exclusive)
+     * @returns {*} query parameter value, converted to the proper type
      * @public
      */
     get: function( key, schemaElement ) {
@@ -31,24 +43,23 @@ window.QueryStringMachine = (function() {
     },
 
     /**
-     * Like `get` but for an arbitrary string, for testing
-     * @param {string} string
-     * @param {string} key
-     * @param {Object} schemaElement, see QueryStringMachine.get
-     * @returns {*}
+     * Like `get` but for an arbitrary parameters string.
+     *
+     * @param {string} string - the parameters string
+     * @param {string} key - the query parameter name
+     * @param {Object} schemaElement - see QueryStringMachine.get
+     * @returns {*} query parameter value, converted to the proper type
      * @public (for-testing)
      */
     getForString: function( string, key, schemaElement ) {
-      var result = parseElement( schemaElement, getValues( string, key ) );
-      validate( schemaElement, result );
-      return result;
+      return parseElement( schemaElement, getValues( string, key ) );
     },
 
     /**
-     * Get query parameter values for every key, using the supplied schemas.
+     * Gets values for every query parameter, using the specified schema.
      *
-     * @param {Object} schema - key is query parameter name and value is the schemaElement (see QueryStringMachine.get)
-     * @returns {Object} - key value pairs holding the parsed results
+     * @param {Object} schema - see QueryStringMachine.getAllForString
+     * @returns {Object} - see QueryStringMachine.getAllForString
      * @public
      */
     getAll: function( schema ) {
@@ -56,11 +67,11 @@ window.QueryStringMachine = (function() {
     },
 
     /**
-     * Get values for each query parameter, using the supplied schemas.
+     * Like `getAll` but for an arbitrary parameters string.
      *
-     * @param string
-     * @param schema
-     * @returns {Object} - key value pairs holding the parsed results
+     * @param string - the parameters string
+     * @param schema - key/value pairs, key is query parameter name and value is a schemaElement
+     * @returns {Object} - key/value pairs holding the parsed results
      * @public (for-testing)
      */
     getAllForString: function( string, schema ) {
@@ -74,93 +85,135 @@ window.QueryStringMachine = (function() {
     }
   };
 
-  // Convenience functions for parsing string to other primitive types
+  /**
+   * Converts a string to a number.
+   * @param string
+   * @returns {number}
+   */
   var stringToNumber = function( string ) {
-    return Number( string );
+    var value = Number( string ); //TODO This looks suspicious. Should this be new Number? Should we be using Number?
+    // Number returns NaN if the string cannot be converted to a number
+    assert && assert( !isNaN( value ), 'illegal value for type number: ' + string );
+    return value; //TODO are we returning number or Number here?
   };
+
+  //TODO is this really necessary?
+  /**
+   * Converts a string to a string.
+   * @param string
+   * @returns {*}
+   */
   var stringToString = function( string ) {
     return string;
   };
+
+  /**
+   * Converts a string to a boolean.
+   * @param string
+   * @returns {boolean}
+   */
   var stringToBoolean = function( string ) {
     assert && assert( string === 'true' || string === 'false', 'illegal value for boolean: ' + string );
-    return string === 'true';
-  };
-
-  var validate = function( schemaElement, value ) {
-    schemaElement.allowedValues && assert && assert( schemaElement.allowedValues.indexOf( value ) >= 0, 'value not allowed: ' + value + ', allowedValues = ' + schemaElement.allowedValues );
-    schemaElement.validate && schemaElement.validate( value );
-    schemaElement.type === 'number' && assert && assert( typeof value === 'number', 'should have been a number' );
+    return ( string === 'true' );
   };
 
   /**
-   * @param schemaElement TODO add type expression and description
-   * @param {Array} values any matches from the query string, could be multiple for ?value=x&value=y for example
-   * @returns {*}
+   * Validates the result of parsing a schemaElement.
+   *
+   * @param value - see value returned by parseElement
+   * @param {Object} schemaElement - see QueryStringMachine.get
+   */
+  var validateResult = function( value, schemaElement ) {
+    if ( assert ) {
+      schemaElement.allowedValues && assert( schemaElement.allowedValues.indexOf( value ) >= 0, 'value not allowed: ' + value + ', allowedValues = ' + schemaElement.allowedValues );
+      schemaElement.validate && schemaElement.validate( value );
+      schemaElement.type === 'number' && assert( typeof value === 'number', 'should have been a number' );
+    }
+  };
+
+  /**
+   * Uses the supplied schemaElement to convert query parameter value(s) from string to the desired value type.
+   *
+   * @param schemaElement - see QueryStringMachine.get
+   * @param {string[]} values - any matches from the query string, could be multiple for ?value=x&value=y for example
+   * @returns {*} the associated value, converted to the proper type
    */
   var parseElement = function( schemaElement, values ) {
-    assert && assert( !(schemaElement.allowedValues && schemaElement.validate), 'cannot specify allowedValues and validate simultaneously' );
 
-    // TODO: make sure schema default value matches schema type
+    assert && assert( !(schemaElement.type && schemaElement.parse), 'type and parse are mutually exclusive' );
+    assert && assert( !(schemaElement.allowedValues && schemaElement.validate), 'allowedValues and validate are mutually exclusive' );
+
+    var value = null;
+
+    // TODO: make sure schemaElement.defaultValue matches schemaElement.type (how to do that if parse is provided instead of type?)
     if ( values.length === 0 ) {
 
+      //TODO Why is flag treated specially here? Why can't it have a defaultValue? If it can't have defaultValue, assert that somewhere.
       // If flag is not supplied, default to false.
       if ( schemaElement.type === 'flag' ) {
-        return false;
+        value = false;
       }
       else {
-        return schemaElement.defaultValue;
+        value = schemaElement.defaultValue;
       }
     }
     else if ( values.length === 1 ) {
-      if ( schemaElement.type === 'number' ) {
-        return stringToNumber( values[ 0 ] );
-      }
-      else if ( schemaElement.type === 'string' ) {
-        return stringToString( values[ 0 ] );
-      }
-      else if ( schemaElement.type === 'boolean' ) {
-        return stringToBoolean( values[ 0 ] );
-      }
-      else if ( schemaElement.type === 'array' ) {
-        var subSchemaElement = {};
-        for ( var k in schemaElement ) {
-          if ( schemaElement.hasOwnProperty( k ) && k !== 'type' ) {
-            subSchemaElement[ k ] = schemaElement;
+      if ( schemaElement.type ) {
+
+        assert && assert( _.contains( VALID_TYPES, schemaElement.type ), 'invalid type: ' + schemaElement.type );
+
+        if ( schemaElement.type === 'number' ) {
+          value = stringToNumber( values[ 0 ] );
+        }
+        else if ( schemaElement.type === 'string' ) {
+          value = stringToString( values[ 0 ] );
+        }
+        else if ( schemaElement.type === 'boolean' ) {
+          value = stringToBoolean( values[ 0 ] );
+        }
+        else if ( schemaElement.type === 'array' ) {
+          var subSchemaElement = {};
+          for ( var k in schemaElement ) {
+            if ( schemaElement.hasOwnProperty( k ) && k !== 'type' ) {
+              subSchemaElement[ k ] = schemaElement;
+            }
+          }
+          assert && assert( schemaElement.elementType, 'array element type must be defined' );
+          subSchemaElement.type = schemaElement.elementType;
+          value = values[ 0 ].split( schemaElement.separator ).map( function( element ) {
+            return parseElement( subSchemaElement, [ element ] );
+          } );
+        }
+        else if ( schemaElement.type === 'flag' ) {
+          if ( values[ 0 ] === null ) {
+            value = true;  // When the value is null, like for ?webgl, default to true
+          }
+          else {
+            value = stringToBoolean( values[ 0 ] );
           }
         }
-        assert && assert( schemaElement.elementType, 'array element type must be defined' );
-        subSchemaElement.type = schemaElement.elementType;
-        return values[ 0 ].split( schemaElement.separator ).map( function( element ) {
-          return parseElement( subSchemaElement, [ element ] );
-        } );
-      }
-      else if ( schemaElement.type === 'flag' ) {
-
-        // When the value is null, like for ?webgl, default to true
-        if ( values[ 0 ] === null ) {
-          return true;
-        }
         else {
-          return stringToBoolean( values[ 0 ] );
+          throw new Error( 'invalid type: ' + schemaElement.type );
         }
-      }
-      else if ( schemaElement.parse ) {
-        return schemaElement.parse( values[ 0 ] );
       }
       else {
-        throw new Error( 'not supported' );
+        value = schemaElement.parse( values[ 0 ] );
       }
     }
     else {
-      // In the future we could add support for the form: ?id=1&id=2&id=7 etc
-      throw new Error( 'Specifying keys multiple times not supported..... yet?' );
+      //TODO add support duplicate parameters, e.g.: ?id=1&id=2&id=7
+      throw new Error( 'duplicate parameters are not currently supported' );
     }
+
+    assert && validateResult( value, schemaElement );
+    return value;
   };
 
   /**
-   * Query strings may show the same key appearing multiple times, such as ?value=2&value=3.  This method recovers all
-   * of the string values.  For this example, it would be ['2','3'].
-   * @param {string} string - the query string
+   * Query strings may show the same key appearing multiple times, such as ?value=2&value=3.
+   * This method recovers all of the string values.  For this example, it would be ['2','3'].
+   *
+   * @param {string} string - the parameters string
    * @param {string} key - the key for which we are finding values.
    * @returns {string[]} - the resulting values
    */
