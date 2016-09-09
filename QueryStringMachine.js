78fe7fbe7a0dab9d840e179ecee4925fa97b927a
samreid <reids@colorado.edu>
2016-09-08 20:55:36 -0600

    Renamed methods and added JSDoc, see https://github.com/phetsims/query-string-machine/issues/1

--- QueryStringMachine.js
+++ QueryStringMachine.js
@@ -1,15 +1,13 @@
 // Copyright 2016, University of Colorado Boulder
 
+/**
+ * Query String parser that supports type coercion, defaults, error checking, etc. based on a schema.
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
 window.QueryStringMachine = (function() {
   'use strict';
 
-  var assert = function( b, text ) {
-    if ( !b ) {
-      console.log( text );
-      throw new Error( text );
-    }
-  };
-
+  // Convenience functions for parsing string to other primitive types
   var stringToNumber = function( string ) {
     return Number( string );
   };
@@ -17,30 +15,23 @@ window.QueryStringMachine = (function() {
     return string;
   };
   var stringToBoolean = function( string ) {
-    assert( string === 'true' || string === 'false', 'illegal value: ' + string );
-    if ( string === 'true' ) {
-      return true;
-    }
-    else if ( string === 'false' ) {
-      return false;
-    }
-    else {
-      throw new Error( 'illegal value: ' + string );
-    }
+    assert && assert( string === 'true' || string === 'false', 'illegal value: ' + string );
+    return string === 'true';
   };
 
   var validate = function( schemaEntry, value ) {
     if ( schemaEntry.allowedValues ) {
-      assert( schemaEntry.allowedValues.indexOf( value ) >= 0, 'value not allowed: ' + value );
+      assert && assert( schemaEntry.allowedValues.indexOf( value ) >= 0, 'value not allowed: ' + value );
     }
     else if ( schemaEntry.validate ) {
       schemaEntry.validate( value );
     }
     else if ( schemaEntry.type === 'number' ) {
-      assert( typeof value === 'number', 'should have been a number' );
+      assert && assert( typeof value === 'number', 'should have been a number' );
     }
     return value;
   };
+
   /**
    *
    * @param key
@@ -49,7 +40,7 @@ window.QueryStringMachine = (function() {
    * @returns {number}
    */
   var parseElement = function( key, schemaEntry, values ) {
-    assert( !(schemaEntry.allowedValues && schemaEntry.validate), 'cannot specify allowedValues and validate simultaneously' );
+    assert && assert( !(schemaEntry.allowedValues && schemaEntry.validate), 'cannot specify allowedValues and validate simultaneously' );
 
     // TODO: make sure schema default value matches schema type
     if ( values.length === 0 ) {
@@ -94,6 +85,13 @@ window.QueryStringMachine = (function() {
     }
   };
 
+  /**
+   * Query strings may show the same key appearing multiple times, such as ?value=2&value=3.  This method recovers all
+   * of the string values.  For this example, it would be ['2','3'].
+   * @param {string} string - the query string
+   * @param {string} key - the key for which we are finding values.
+   * @returns {string[]} - the resulting values
+   */
   var getValues = function( string, key ) {
     var values = [];
     var params = string.slice( 1 ).split( '&' );
@@ -112,16 +110,43 @@ window.QueryStringMachine = (function() {
   };
   var QueryStringMachine = {
 
-    // Just invoke for a single value
+    /**
+     * Returns the value for a single element.
+     * @param key
+     * @param {object} schemaElement * required: 'type' or 'parse'
+     *                                   * type: one of 'boolean'|'number'|'string'|'flag'
+     *                                   * parse: a function that takes a string and returns an object
+     *                               * optional: 'defaultValue' - The value to take if no query parameter is provided
+     *                               * optional: 'allowedValues' - Array of the allowed values for validation
+     *                               * optional: 'validate' - function that takes a parsed object (not string) and checks if it is acceptable
+     *
+     * @returns {number}
+     * @public
+     */
     get: function( key, schemaElement ) {
       return parseElement( key, schemaElement, getValues( window.location.search, key ) );
     },
-    parse: function( schema ) {
-      return this.parseString( window.location.search, schema );
+
+    /**
+     * Get query parameter values for every key, using the supplied schemas.
+     *
+     * @param {Object} schema - key is query parameter name and value is the schemaElement (see QueryStringMachine.get)
+     * @returns {Object} - key value pairs holding the parsed results
+     * @public
+     */
+    getAll: function( schema ) {
+      return this.getAllForString( window.location.search, schema );
     },
 
-    // Return an object hash with keys matching schema
-    parseString: function( string, schema ) {
+    /**
+     * Get values for each query parameter, using the supplied schemas.
+     *
+     * @param string
+     * @param schema
+     * @returns {Object} - key value pairs holding the parsed results
+     * @public (for-testing)
+     */
+    getAllForString: function( string, schema ) {
       var result = {};
       for ( var key in schema ) {
         if ( schema.hasOwnProperty( key ) ) {
