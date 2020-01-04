ad6f0ede90edccba78a8791f5f45bfca5a5ff663
phet-dev <phetadmin@gmail.com>
2020-01-04 03:24:51 -0700

    update copyright dates from daily grunt work

--- js/QueryStringMachine.js
+++ js/QueryStringMachine.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2019, University of Colorado Boulder
+// Copyright 2016-2020, University of Colorado Boulder
 
 /**
  * Query String parser that supports type coercion, defaults, error checking, etc. based on a schema.
