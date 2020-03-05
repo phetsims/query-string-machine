65149524afac2a3b2eb08f8f2089a7af1633bb35
samreid <reids@colorado.edu>
2020-03-04 21:05:33 -0700

    Migrate tests and HTML to es6 modules, see https://github.com/phetsims/query-string-machine/issues/38

--- js/QueryStringMachineTests.js
+++ js/QueryStringMachineTests.js
@@ -1,3 +1,4 @@
+
 // Copyright 2016-2019, University of Colorado Boulder
 
 /**
@@ -5,121 +6,132 @@
  *
  * @author Sam Reid (PhET Interactive Simulations)
  */
-define( require => {
-  'use strict';
 
-  QUnit.module( 'QueryStringMachine' );
 
-  // assert shadows window.assert
-  QUnit.test( 'basic tests', function( assert ) {
-    const value = 'hello';
-    assert.equal( value, 'hello', 'We expect value to be hello' );
+QUnit.module( 'QueryStringMachine' );
 
-    const schemaMap = {
-      height: {
-        type: 'number',
-        defaultValue: 6,
-        validValues: [ 4, 5, 6, 7, 8 ]
-      },
-      name: {
-        type: 'string',
-        defaultValue: 'Larry',
-        isValidValue: function( str ) {
-          return ( str.indexOf( 'Z' ) !== 0 ); // Name cannot start with 'Z'
-        }
-      },
-      custom: {
-        type: 'custom',
-        defaultValue: 'abc',
-        validValues: [ 'abc', 'def', 'ghi' ],
-        parse: function( string ) {
-          return string.toLowerCase();
-        }
-      },
-      isWebGL: {
-        type: 'flag'
-      },
-      screens: {
+// assert shadows window.assert
+QUnit.test( 'basic tests', function( assert ) {
+  const value = 'hello';
+  assert.equal( value, 'hello', 'We expect value to be hello' );
+
+  const schemaMap = {
+    height: {
+      type: 'number',
+      defaultValue: 6,
+      validValues: [ 4, 5, 6, 7, 8 ]
+    },
+    name: {
+      type: 'string',
+      defaultValue: 'Larry',
+      isValidValue: function( str ) {
+        return ( str.indexOf( 'Z' ) !== 0 ); // Name cannot start with 'Z'
+      }
+    },
+    custom: {
+      type: 'custom',
+      defaultValue: 'abc',
+      validValues: [ 'abc', 'def', 'ghi' ],
+      parse: function( string ) {
+        return string.toLowerCase();
+      }
+    },
+    isWebGL: {
+      type: 'flag'
+    },
+    screens: {
+      type: 'array',
+      defaultValue: [],
+      elementSchema: {
+        type: 'number'
+      }
+    },
+    colors: {
+      type: 'array',
+      defaultValue: [ 'red', 'green', 'blue' ],
+      elementSchema: {
+        type: 'string'
+      }
+    }
+  };
+
+  assert.deepEqual( QueryStringMachine.getAllForString( schemaMap, '' ), {
+    'height': 6,
+    'name': 'Larry',
+    'custom': 'abc',
+    'isWebGL': false,
+    'screens': [],
+    'colors': [ 'red', 'green', 'blue' ]
+  }, 'A blank query string should provide defaults' );
+
+  assert.deepEqual( QueryStringMachine.getAllForString( schemaMap, '?height=7&isWebGL' ), {
+    'height': 7,
+    'name': 'Larry',
+    'custom': 'abc',
+    'isWebGL': true,
+    'screens': [],
+    'colors': [ 'red', 'green', 'blue' ]
+  }, 'Query parameter values should be parsed' );
+
+  assert.deepEqual( QueryStringMachine.getAllForString( schemaMap, '?height=7&isWebGL&custom=DEF' ), {
+    'height': 7,
+    'name': 'Larry',
+    'custom': 'def',
+    'isWebGL': true,
+    'screens': [],
+    'colors': [ 'red', 'green', 'blue' ]
+  }, 'Custom query parameter should be supported' );
+
+  assert.deepEqual( QueryStringMachine.getAllForString( schemaMap, '?isWebGL&screens=1,2,3,5&colors=yellow,orange,pink' ), {
+    'height': 6,
+    'name': 'Larry',
+    'custom': 'abc',
+    'isWebGL': true,
+    'screens': [ 1, 2, 3, 5 ],
+    'colors': [ 'yellow', 'orange', 'pink' ]
+  }, 'Array should be parsed' );
+
+  // Test that isValidValue is supported for arrays with a contrived check (element sum == 7).
+  // With an input of [2,4,0], QSM should throw an error, and it should be caught here.
+  assert.throws( function() {
+    QueryStringMachine.getAllForString( {
+      numbers: {
         type: 'array',
-        defaultValue: [],
         elementSchema: {
           type: 'number'
-        }
-      },
-      colors: {
-        type: 'array',
-        defaultValue: [ 'red', 'green', 'blue' ],
-        elementSchema: {
-          type: 'string'
+        },
+        defaultValue: [ 1, 6, 0 ],
+        isValidValue: function( arr ) {
+          // Fake test: check that elements sum to 7 for phetsims/query-string-machine#11
+          const arraySum = arr.reduce( function( a, b ) { return a + b; }, 0 );
+          return ( arraySum === 7 );
         }
       }
-    };
+    }, '?numbers=2,4,0' );
+  }, 'Array error handling should catch exception' );
 
-    assert.deepEqual( QueryStringMachine.getAllForString( schemaMap, '' ), {
-      'height': 6,
-      'name': 'Larry',
-      'custom': 'abc',
-      'isWebGL': false,
-      'screens': [],
-      'colors': [ 'red', 'green', 'blue' ]
-    }, 'A blank query string should provide defaults' );
+  assert.throws( function() {
+    QueryStringMachine.getAllForString( {
+      sim: {
+        type: 'string'
+      }
+    }, '?ea&hello=true' );
 
-    assert.deepEqual( QueryStringMachine.getAllForString( schemaMap, '?height=7&isWebGL' ), {
-      'height': 7,
-      'name': 'Larry',
-      'custom': 'abc',
-      'isWebGL': true,
-      'screens': [],
-      'colors': [ 'red', 'green', 'blue' ]
-    }, 'Query parameter values should be parsed' );
+  }, 'Catch missing required query parameter' );
 
-    assert.deepEqual( QueryStringMachine.getAllForString( schemaMap, '?height=7&isWebGL&custom=DEF' ), {
-      'height': 7,
-      'name': 'Larry',
-      'custom': 'def',
-      'isWebGL': true,
-      'screens': [],
-      'colors': [ 'red', 'green', 'blue' ]
-    }, 'Custom query parameter should be supported' );
+  assert.deepEqual( QueryStringMachine.getForString( 'hello', {
+    type: 'array',
+    elementSchema: {
+      type: 'number'
+    },
+    validValues: [
+      [ 1, 2 ], [ 3, 4 ], [ 1, 2, 3 ]
+    ],
+    defaultValue: [ 1, 2 ]
+  }, '?ea&hello=1,2,3' ), [ 1, 2, 3 ], 'Arrays should support defaultValue and validValues' );
 
-    assert.deepEqual( QueryStringMachine.getAllForString( schemaMap, '?isWebGL&screens=1,2,3,5&colors=yellow,orange,pink' ), {
-      'height': 6,
-      'name': 'Larry',
-      'custom': 'abc',
-      'isWebGL': true,
-      'screens': [ 1, 2, 3, 5 ],
-      'colors': [ 'yellow', 'orange', 'pink' ]
-    }, 'Array should be parsed' );
-
-    // Test that isValidValue is supported for arrays with a contrived check (element sum == 7).
-    // With an input of [2,4,0], QSM should throw an error, and it should be caught here.
-    assert.throws( function() {
-      QueryStringMachine.getAllForString( {
-        numbers: {
-          type: 'array',
-          elementSchema: {
-            type: 'number'
-          },
-          defaultValue: [ 1, 6, 0 ],
-          isValidValue: function( arr ) {
-            // Fake test: check that elements sum to 7 for phetsims/query-string-machine#11
-            const arraySum = arr.reduce( function( a, b ) { return a + b; }, 0 );
-            return ( arraySum === 7 );
-          }
-        }
-      }, '?numbers=2,4,0' );
-    }, 'Array error handling should catch exception' );
-
-    assert.throws( function() {
-      QueryStringMachine.getAllForString( {
-        sim: {
-          type: 'string'
-        }
-      }, '?ea&hello=true' );
-
-    }, 'Catch missing required query parameter' );
-
-    assert.deepEqual( QueryStringMachine.getForString( 'hello', {
+  assert.throws( function() {
+    QueryStringMachine.getForString( 'hello', {
       type: 'array',
       elementSchema: {
         type: 'number'
@@ -128,79 +140,66 @@ define( require => {
         [ 1, 2 ], [ 3, 4 ], [ 1, 2, 3 ]
       ],
       defaultValue: [ 1, 2 ]
-    }, '?ea&hello=1,2,3' ), [ 1, 2, 3 ], 'Arrays should support defaultValue and validValues' );
+    }, '?ea&hello=1,2,3,99' );
+  }, 'Catch invalid value for array' );
 
-    assert.throws( function() {
-      QueryStringMachine.getForString( 'hello', {
-        type: 'array',
-        elementSchema: {
-          type: 'number'
-        },
-        validValues: [
-          [ 1, 2 ], [ 3, 4 ], [ 1, 2, 3 ]
-        ],
-        defaultValue: [ 1, 2 ]
-      }, '?ea&hello=1,2,3,99' );
-    }, 'Catch invalid value for array' );
+  assert.deepEqual( QueryStringMachine.getForString( 'screens', {
+    type: 'array',
+    elementSchema: {
+      type: 'number'
+    },
+    defaultValue: null
+  }, '?screens=1,2,3' ), [ 1, 2, 3 ], 'Test array of numbers' );
 
-    assert.deepEqual( QueryStringMachine.getForString( 'screens', {
-      type: 'array',
-      elementSchema: {
-        type: 'number'
-      },
-      defaultValue: null
-    }, '?screens=1,2,3' ), [ 1, 2, 3 ], 'Test array of numbers' );
+} );
 
-  } );
+// Tests for our own deepEquals method
+QUnit.test( 'deepEquals', function( assert ) {
+  assert.equal( QueryStringMachine.deepEquals( 7, 7 ), true, '7 should equal itself' );
+  assert.equal( QueryStringMachine.deepEquals( 7, 8 ), false, '7 should not equal 8' );
+  assert.equal( QueryStringMachine.deepEquals( 7, '7' ), false, '7 should not equal "7"' );
+  assert.equal( QueryStringMachine.deepEquals( { 0: 'A' }, 'A' ), false, 'string tests' );
+  assert.equal( QueryStringMachine.deepEquals( [ 'hello', 7 ], [ 'hello', 7 ] ), true, 'array equality test' );
+  assert.equal( QueryStringMachine.deepEquals( [ 'hello', 7 ], [ 'hello', '7' ] ), false, 'array inequality test' );
+  assert.equal( QueryStringMachine.deepEquals( [ 'hello', { hello: true } ], [ 'hello', { hello: true } ] ), true, 'object in array inequality test' );
+  assert.equal( QueryStringMachine.deepEquals( [ 'hello', { hello: true } ], [ 'hello', { hello: false } ] ), false, 'object in array  inequality test' );
+  assert.equal( QueryStringMachine.deepEquals( { x: [ { y: 'hello' }, true, 123, 'x' ] }, { x: [ { y: 'hello' }, true, 123, 'x' ] } ), true, 'object in array  inequality test' );
+  assert.equal( QueryStringMachine.deepEquals( { x: [ { y: 'hello' }, true, 123, 'x' ] }, { x: [ true, { y: 'hello' }, true, 123, 'x' ] } ), false, 'object in array  inequality test' );
+  assert.equal( QueryStringMachine.deepEquals( { x: [ { y: 'hello' }, true, 123, 'x' ] }, { y: [ { y: 'hello' }, true, 123, 'x' ] } ), false, 'object in array  inequality test' );
+  assert.equal( QueryStringMachine.deepEquals( null, null ), true, 'null null' );
+  assert.equal( QueryStringMachine.deepEquals( null, undefined ), false, 'null undefined' );
+  assert.equal( QueryStringMachine.deepEquals( undefined, undefined ), true, 'undefined undefined' );
+  assert.equal( QueryStringMachine.deepEquals( function() {}, function() {} ), false, 'different implementations of similar functions' );
+  const f = function() {};
+  assert.equal( QueryStringMachine.deepEquals( f, f ), true, 'same reference function' );
+} );
 
-  // Tests for our own deepEquals method
-  QUnit.test( 'deepEquals', function( assert ) {
-    assert.equal( QueryStringMachine.deepEquals( 7, 7 ), true, '7 should equal itself' );
-    assert.equal( QueryStringMachine.deepEquals( 7, 8 ), false, '7 should not equal 8' );
-    assert.equal( QueryStringMachine.deepEquals( 7, '7' ), false, '7 should not equal "7"' );
-    assert.equal( QueryStringMachine.deepEquals( { 0: 'A' }, 'A' ), false, 'string tests' );
-    assert.equal( QueryStringMachine.deepEquals( [ 'hello', 7 ], [ 'hello', 7 ] ), true, 'array equality test' );
-    assert.equal( QueryStringMachine.deepEquals( [ 'hello', 7 ], [ 'hello', '7' ] ), false, 'array inequality test' );
-    assert.equal( QueryStringMachine.deepEquals( [ 'hello', { hello: true } ], [ 'hello', { hello: true } ] ), true, 'object in array inequality test' );
-    assert.equal( QueryStringMachine.deepEquals( [ 'hello', { hello: true } ], [ 'hello', { hello: false } ] ), false, 'object in array  inequality test' );
-    assert.equal( QueryStringMachine.deepEquals( { x: [ { y: 'hello' }, true, 123, 'x' ] }, { x: [ { y: 'hello' }, true, 123, 'x' ] } ), true, 'object in array  inequality test' );
-    assert.equal( QueryStringMachine.deepEquals( { x: [ { y: 'hello' }, true, 123, 'x' ] }, { x: [ true, { y: 'hello' }, true, 123, 'x' ] } ), false, 'object in array  inequality test' );
-    assert.equal( QueryStringMachine.deepEquals( { x: [ { y: 'hello' }, true, 123, 'x' ] }, { y: [ { y: 'hello' }, true, 123, 'x' ] } ), false, 'object in array  inequality test' );
-    assert.equal( QueryStringMachine.deepEquals( null, null ), true, 'null null' );
-    assert.equal( QueryStringMachine.deepEquals( null, undefined ), false, 'null undefined' );
-    assert.equal( QueryStringMachine.deepEquals( undefined, undefined ), true, 'undefined undefined' );
-    assert.equal( QueryStringMachine.deepEquals( function() {}, function() {} ), false, 'different implementations of similar functions' );
-    const f = function() {};
-    assert.equal( QueryStringMachine.deepEquals( f, f ), true, 'same reference function' );
-  } );
+QUnit.test( 'removeKeyValuePair', function( assert ) {
+  assert.equal( QueryStringMachine.removeKeyValuePair( '?time=now', 'time' ), '', 'Remove single occurrence' );
+  assert.equal( QueryStringMachine.removeKeyValuePair( '?time=now&place=here', 'time' ), '?place=here', 'Remove single occurrence but leave other' );
+  assert.equal( QueryStringMachine.removeKeyValuePair( '?time=now&time=later', 'time' ), '', 'Remove multiple occurrences' );
+  assert.equal( QueryStringMachine.removeKeyValuePair( '?time=now&time', 'time' ), '', 'Remove multiple occurrences, one with value' );
+  assert.equal( QueryStringMachine.removeKeyValuePair( '?place=here&time=now', 'time' ), '?place=here', 'Different order' );
+  assert.equal( QueryStringMachine.removeKeyValuePair( '?time&place', 'time' ), '?place', 'Remove with no values' );
+  assert.equal( QueryStringMachine.removeKeyValuePair( '?place&time', 'time' ), '?place', 'Remove with no values' );
+  assert.equal( QueryStringMachine.removeKeyValuePair( '?place&time', 'times' ), '?place&time', 'Key to remove not present' );
+  assert.equal( QueryStringMachine.removeKeyValuePair(
+    '?sim=ohms-law&phetioValidateTandems&phetioDebug&phetioValidateAPI=false', 'fuzz' ),
+    '?sim=ohms-law&phetioValidateTandems&phetioDebug&phetioValidateAPI=false',
+    'Key to remove not present' );
+} );
 
-  QUnit.test( 'removeKeyValuePair', function( assert ) {
-    assert.equal( QueryStringMachine.removeKeyValuePair( '?time=now', 'time' ), '', 'Remove single occurrence' );
-    assert.equal( QueryStringMachine.removeKeyValuePair( '?time=now&place=here', 'time' ), '?place=here', 'Remove single occurrence but leave other' );
-    assert.equal( QueryStringMachine.removeKeyValuePair( '?time=now&time=later', 'time' ), '', 'Remove multiple occurrences' );
-    assert.equal( QueryStringMachine.removeKeyValuePair( '?time=now&time', 'time' ), '', 'Remove multiple occurrences, one with value' );
-    assert.equal( QueryStringMachine.removeKeyValuePair( '?place=here&time=now', 'time' ), '?place=here', 'Different order' );
-    assert.equal( QueryStringMachine.removeKeyValuePair( '?time&place', 'time' ), '?place', 'Remove with no values' );
-    assert.equal( QueryStringMachine.removeKeyValuePair( '?place&time', 'time' ), '?place', 'Remove with no values' );
-    assert.equal( QueryStringMachine.removeKeyValuePair( '?place&time', 'times' ), '?place&time', 'Key to remove not present' );
-    assert.equal( QueryStringMachine.removeKeyValuePair(
-      '?sim=ohms-law&phetioValidateTandems&phetioDebug&phetioValidateAPI=false', 'fuzz' ),
-      '?sim=ohms-law&phetioValidateTandems&phetioDebug&phetioValidateAPI=false',
-      'Key to remove not present' );
-  } );
+QUnit.test( 'appendQueryString', function( assert ) {
 
-  QUnit.test( 'appendQueryString', function( assert ) {
+  const test = function( url, queryParameters, expected ) {
+    assert.equal( QueryStringMachine.appendQueryString( url, queryParameters ), expected, url + ' + ' + queryParameters + ' should be ok' );
+  };
 
-    const test = function( url, queryParameters, expected ) {
-      assert.equal( QueryStringMachine.appendQueryString( url, queryParameters ), expected, url + ' + ' + queryParameters + ' should be ok' );
-    };
-
-    test( 'http://localhost.com/hello.html', '', 'http://localhost.com/hello.html' );
-    test( 'http://localhost.com/hello.html?hi', '', 'http://localhost.com/hello.html?hi' );
-    test( 'http://localhost.com/hello.html', '?test', 'http://localhost.com/hello.html?test' );
-    test( 'http://localhost.com/hello.html', '&test', 'http://localhost.com/hello.html?test' );
-    test( 'http://localhost.com/hello.html?abc', '', 'http://localhost.com/hello.html?abc' );
-    test( 'http://localhost.com/hello.html?abc', '?123', 'http://localhost.com/hello.html?abc&123' );
-    test( 'http://localhost.com/hello.html?abc', '&123', 'http://localhost.com/hello.html?abc&123' );
-  } );
+  test( 'http://localhost.com/hello.html', '', 'http://localhost.com/hello.html' );
+  test( 'http://localhost.com/hello.html?hi', '', 'http://localhost.com/hello.html?hi' );
+  test( 'http://localhost.com/hello.html', '?test', 'http://localhost.com/hello.html?test' );
+  test( 'http://localhost.com/hello.html', '&test', 'http://localhost.com/hello.html?test' );
+  test( 'http://localhost.com/hello.html?abc', '', 'http://localhost.com/hello.html?abc' );
+  test( 'http://localhost.com/hello.html?abc', '?123', 'http://localhost.com/hello.html?abc&123' );
+  test( 'http://localhost.com/hello.html?abc', '&123', 'http://localhost.com/hello.html?abc&123' );
 } );
--- js/query-string-machine-tests.js
+++ js/query-string-machine-tests.js
@@ -5,12 +5,8 @@
  *
  * @author Sam Reid (PhET Interactive Simulations)
  */
-define( require => {
-  'use strict';
 
-  // modules
-  require( './QueryStringMachineTests' ); // Use local path because there is no path for query string machine, because it is a preload
+import './QueryStringMachineTests.js'; // Use local path because there is no path for query string machine, because it is a preload
 
-  // Since our tests are loaded asynchronously, we must direct QUnit to begin the tests
-  QUnit.start();
-} );
+// Since our tests are loaded asynchronously, we must direct QUnit to begin the tests
+QUnit.start();
