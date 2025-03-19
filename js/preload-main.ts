6f5669c242298009ce64f01bd7ccd4b4bb53eee6
Michael Kauzmann <michael.kauzmann@colorado.edu>
2025-03-19 15:04:15 -0600

    type-check fix, https://github.com/phetsims/query-string-machine/issues/45

--- js/preload-main.ts
+++ js/preload-main.ts
@@ -6,5 +6,4 @@
 
 import QueryStringMachineModule from './QueryStringMachineModule.js';
 
-// @ts-expect-error - QueryStringMachineModule has more correct types, but I don't want to expose that to the whole codebase on a friday afternoon, https://github.com/phetsims/query-string-machine/issues/45
 self.QueryStringMachine = QueryStringMachineModule;
